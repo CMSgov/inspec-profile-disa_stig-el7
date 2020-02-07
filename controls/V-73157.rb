923e7036a787a3f6be29a9c5e689b948b1f856c5
Eugene Aronne <eugene.aronne@cms.hhs.gov>
2020-02-07 15:39:45 -0500

    Correct typo idle_delay to idle-delay
    for V-73157.rb

--- controls/V-73157.rb
+++ controls/V-73157.rb
@@ -76,7 +76,7 @@ Add the setting to lock the session idle delay:
   tag "fix_id": "F-79603r1_fix"
 
   # @todo - dynamically gather system_db_path?
-  describe command("grep -i idle_delay #{system_db_path}/locks/*") do
+  describe command("grep -i idle-delay #{system_db_path}/locks/*") do
     its('stdout') { should_not match %r{^$} }
   end if package('gnome-desktop3').installed?
   describe "The GNOME desktop is not installed" do
